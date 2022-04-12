0a89f74159af01bc33ad21b430761f3b689fa7cd
Kimberly Miller <millerk10@moravian.edu>
2022-04-12 12:05:29 -0400

    Removed future code

--- attachments_generator.py
+++ attachments_generator.py
@@ -45,6 +45,7 @@ class AttachmentsGenerator:
                 job_count['num_jobs'] += 1
                 database.lpush('num_jobs_documents_queue', json.dumps(job_count))
 
+
 if __name__ == '__main__':
     database = redis.Redis()
     while not is_redis_available(database):
@@ -54,34 +55,34 @@ if __name__ == '__main__':
     job_queue = JobQueue(database)
     generator = AttachmentsGenerator(job_queue, database)
 
-documents_lst = ["https://api.regulations.gov/v4/documents/EPA-HQ-OA-2003-0003-0003",
+    documents_lst = ["https://api.regulations.gov/v4/documents/EPA-HQ-OA-2003-0003-0003",
                         "https://api.regulations.gov/v4/documents/APHIS-2005-0003-0008",
                         "https://api.regulations.gov/v4/documents/EPA-HQ-OAR-2001-0006-0123"]
-comments_lst = ["https://api.regulations.gov/v4/comments/EPA-HQ-OPP-2003-0132-0596",
+    comments_lst = ["https://api.regulations.gov/v4/comments/EPA-HQ-OPP-2003-0132-0596",
                         "https://api.regulations.gov/v4/comments/EPA-HQ-OPP-2003-0132-0274",
                         "https://api.regulations.gov/v4/comments/EPA-HQ-OAR-2002-0056-0366"]
 
-document_index = 0
-comments_index = 0
-print(database.llen('jobs_waiting_queue'))
-for x in range(10):
-    if x == 0 or x == 1 or x == 2:
-        job = generator.add_job('comments', comments_lst[comments_index])
-        # generator.add_job_type_counter(job, database)
-        comments_index += 1
+    document_index = 0
+    comments_index = 0
+    print(database.llen('jobs_waiting_queue'))
+    for x in range(10):
+        if x == 0 or x == 1 or x == 2:
+            job = generator.add_job('comments', comments_lst[comments_index])
+            #generator.add_job_type_counter(job, database)
+            comments_index += 1
 
-    elif x == 3 or x == 4 or x == 5:
-        job = generator.add_job('documents', documents_lst[document_index])
-        # generator.add_job_type_counter(job, database)
-        document_index += 1
+        elif x == 3 or x == 4 or x == 5:
+            job = generator.add_job('documents', documents_lst[document_index])
+            #generator.add_job_type_counter(job, database)
+            document_index += 1
 
-    elif x == 6:
-        job = generator.add_job('dockets', 'https://api.regulations.gov/v4/dockets/NCUA-2021-0112')
-        # generator.add_job_type_counter(job, database)
+        elif x == 6:
+            job = generator.add_job('dockets', 'https://api.regulations.gov/v4/dockets/NCUA-2021-0112')
+            #generator.add_job_type_counter(job, database)
 
-    elif x == 7 or x == 8 or x == 9:
-        job = generator.add_job('attachments', 'none')
-        # generator.add_job_type_counter(job, database)
-    database.lpush('jobs_waiting_queue', json.dumps(job))
+        elif x == 7 or x == 8 or x == 9:
+            job = generator.add_job('attachments', 'none')
+            #generator.add_job_type_counter(job, database)
+        database.lpush('jobs_waiting_queue', json.dumps(job))
 
-
+    print(database.llen('jobs_waiting_queue'))
